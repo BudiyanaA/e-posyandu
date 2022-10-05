4be63e124cc937e4c2e7f8dbe88c41218bb221ce
budiyanaa <asepbudiyana12@gmail.com>
2022-10-05 09:35:20 +0700

    config database server

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "deriz13",
-    DB: "e-posyandu",
+    HOST: "ec2-44-193-178-122.compute-1.amazonaws.com",
+    USER: "mmyrvmkwlnaelz",
+    PASSWORD: "3e27155fac3374c1fffc84b498563d0841bf89d28922fd0cb1bbfd0d04b9c8c9",
+    DB: "dmbauaaufsa0l",
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },  
     pool: {
       max: 5,
       min: 0,
